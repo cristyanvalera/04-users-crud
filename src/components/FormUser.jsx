06c0d8c44a262af0d05b22cc25d614baae9be5fd
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-02-10 19:00:10 -0400

    wip

--- src/components/FormUser.jsx
+++ src/components/FormUser.jsx
@@ -24,35 +24,38 @@ export const FormUser = ({ createUser, editUser, updateUser, setEditUser }) => {
     };
 
     return (
-        <form onSubmit={handleSubmit(submit)}>
-            <div>
-                <label htmlFor="email">Email</label>
-                <input {...register('email')} id="email" type="email" />
-            </div>
+        <article>
+            <form onSubmit={handleSubmit(submit)}>
+                <h2>Nuevo Usuario</h2>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input {...register('email')} id="email" type="email" />
+                </div>
 
-            <div>
-                <label htmlFor="password">Password</label>
-                <input {...register('password')} id="password" type="password" />
-            </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input {...register('password')} id="password" type="password" />
+                </div>
 
-            <div>
-                <label htmlFor="first_name">Firstname</label>
-                <input {...register('first_name')} id="first_name" type="texto" />
-            </div>
+                <div>
+                    <label htmlFor="first_name">Firstname</label>
+                    <input {...register('first_name')} id="first_name" type="texto" />
+                </div>
 
-            <div>
-                <label htmlFor="last_name">Lastname</label>
-                <input {...register('last_name')} id="last_name" type="texto" />
-            </div>
+                <div>
+                    <label htmlFor="last_name">Lastname</label>
+                    <input {...register('last_name')} id="last_name" type="texto" />
+                </div>
 
-            <div>
-                <label htmlFor="birthday">Birthday</label>
-                <input {...register('birthday')} id="birthday" type="date" />
-            </div>
+                <div>
+                    <label htmlFor="birthday">Birthday</label>
+                    <input {...register('birthday')} id="birthday" type="date" />
+                </div>
 
-            <div>
-                <button>Save</button>
-            </div>
-        </form>
+                <div>
+                    <button>Save</button>
+                </div>
+            </form>
+        </article>
     );
 };
