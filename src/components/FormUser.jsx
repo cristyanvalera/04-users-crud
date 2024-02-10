21a27a006cc9e9f82db1a0887d4da43111262a2c
Cristyan Valera <cristyan.valera.81@gmail.com>
2024-02-09 22:58:51 -0400

    add form user

--- src/components/FormUser.jsx
+++ src/components/FormUser.jsx
@@ -1,29 +1,38 @@
-export const FormUser = () => {
+import { useForm } from "react-hook-form";
+
+export const FormUser = ({ createUser }) => {
+    const { handleSubmit, register, reset } = useForm();
+
+    const submit = (data) => {
+        createUser('/users/', data);
+        reset();
+    };
+
     return (
-        <form>
+        <form onSubmit={handleSubmit(submit)}>
             <div>
                 <label htmlFor="email">Email</label>
-                <input id="email" type="email" />
+                <input {...register('email')} id="email" type="email" />
             </div>
 
             <div>
                 <label htmlFor="password">Password</label>
-                <input id="password" type="password" />
+                <input {...register('password')} id="password" type="password" />
             </div>
 
             <div>
                 <label htmlFor="first_name">Firstname</label>
-                <input id="first_name" type="texto" />
+                <input {...register('first_name')} id="first_name" type="texto" />
             </div>
 
             <div>
                 <label htmlFor="last_name">Lastname</label>
-                <input id="last_name" type="texto" />
+                <input {...register('last_name')} id="last_name" type="texto" />
             </div>
 
             <div>
                 <label htmlFor="birthday">Birthday</label>
-                <input id="birthday" type="date" />
+                <input {...register('birthday')} id="birthday" type="date" />
             </div>
 
             <div>
